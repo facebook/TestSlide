e290e027ec48d9677a519aa1f899bb5d6162ecf9
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2018-10-03 15:48:46 +0100

    Bump version to 1.0.0rc2

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = 'Facebook, Inc. and its affiliates.'
 author = 'Fabio Pugliese Ornellas & Contributors'
 
 # The short X.Y version
-version = '1.0.0rc1'
+version = '1.0.0rc2'
 # The full version, including alpha/beta/rc tags
 release = version
 
