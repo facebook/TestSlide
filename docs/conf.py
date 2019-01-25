91845ea869394666783abff9df0ff4d867394709
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-01-25 13:45:49 +0000

    v1.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = 'Facebook, Inc. and its affiliates.'
 author = 'Fabio Pugliese Ornellas & Contributors'
 
 # The short X.Y version
-version = '1.3.1'
+version = '1.3.2'
 # The full version, including alpha/beta/rc tags
 release = version
 
