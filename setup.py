9300849788b67794906ecc0ec4c8a67d78bef965
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-03-07 11:58:27 +0000

    1.4.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from io import open
 
 setup(
     name="TestSlide",
-    version="1.4.5",
+    version="1.4.6",
     packages=["testslide"],
     maintainer="Fabio Pugliese Ornellas",
     maintainer_email="fabio.ornellas@gmail.com",
