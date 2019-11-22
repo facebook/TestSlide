87f2be70ca57633dc4b4effc13a02ee92be76d08
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-11-22 14:14:17 +0000

    Add license header

--- tests/sample_module.py
+++ tests/sample_module.py
@@ -1,3 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 def test_function(arg1, arg2, kwarg1=None, kwarg2=None):
     "This function is used by some unit tests only"
     return "original response"
