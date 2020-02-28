ac0584bf0f5f01b99cbee794a473c97b3f344967
Balint Csergo <death0wl@fb.com>
2020-02-28 14:26:42 +0000

    flake

--- tests/matchers_unittest.py
+++ tests/matchers_unittest.py
@@ -3,11 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import asyncio
-import csv
 import testslide
-import unittest
-import sys
 from . import sample_module
 
 
