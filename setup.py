e868c307ca923a07d54b02d07c3225cac8582ba3
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-03-16 14:49:13 +0000

    2.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", encoding="utf8") as f:
 
 setup(
     name="TestSlide",
-    version="2.3.0",
+    version="2.3.1",
     packages=["testslide"],
     maintainer="Fabio Pugliese Ornellas",
     maintainer_email="fabio.ornellas@gmail.com",
