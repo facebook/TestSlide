eb136028cf214bbb53bd01ea3e2ac1ff9324b6f5
John Oxley <joxley@fb.com>
2020-03-25 10:23:41 -0700

    Fix up typing imports

--- testslide/matchers.py
+++ testslide/matchers.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import re
-from typing import List, Dict, TypeVar
+from typing import Callable, Dict, List, TypeVar
 
 
 class AlreadyChainedException(Exception):
