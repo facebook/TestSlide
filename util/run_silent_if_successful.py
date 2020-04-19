25209aeab2723f7f12f052d0d289b4379895e09c
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-04-19 14:33:26 +0100

    Switch to Popen

--- util/run_silent_if_successful.py
+++ util/run_silent_if_successful.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
-import pty, sys, subprocess, os, threading
+import os
+import pty
+import signal
+import subprocess
+import sys
+import threading
 
 
 master_pty_fd, slave_pty_fd = pty.openpty()
 read_data = []
+returncode = None
 
 
 def master_pty_fd_reader():
@@ -24,15 +30,17 @@ master_pty_fd_reader_thread = threading.Thread(target=master_pty_fd_reader)
 
 master_pty_fd_reader_thread.start()
 
-completed_process = subprocess.run(
+popen = subprocess.Popen(
     sys.argv[1:], stdin=subprocess.DEVNULL, stdout=slave_pty_fd, stderr=slave_pty_fd,
 )
+
+returncode = popen.wait()
 
 os.close(slave_pty_fd)
 
 master_pty_fd_reader_thread.join()
 
-if completed_process.returncode:
+if returncode:
     for data in read_data:
         os.write(sys.stdout.fileno(), data)
-    sys.exit(completed_process.returncode)
+    sys.exit(returncode)
