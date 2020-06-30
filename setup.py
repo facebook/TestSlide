6169acdcbc24c7407d2fe0856b08725bbacb210e
Balint Csergo <death0wl@fb.com>
2020-06-30 12:49:46 +0100

    bump typeguard dependency version to eliminate erroneous TypeError when trying to check against non-runtime typing.Protocol

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     setup_requires=["setuptools>=38.6.0"],
-    install_requires=["psutil>=5.6.7", "typeguard>=2.7.1",],
+    install_requires=["psutil>=5.6.7", "typeguard>=2.9.1",],
     extras_require={
         "build": [
             "black",
