a977e9b400d6a01af5da29530a9a4b5d04ff7cfa
Balint Csergo <death0wl@fb.com>
2020-09-27 15:49:02 +0100

    typing for import_profiler

--- testslide/import_profiler.py
+++ testslide/import_profiler.py
@@ -5,28 +5,29 @@
 # LICENSE file in the root directory of this source tree.
 
 import time
-
+from typing import Any, Dict, List, Optional, Tuple
+from types import TracebackType
 
 class ImportedModule(object):
     """
     A module that was imported with __import__.
     """
 
-    def __init__(self, name, globals, level, parent=None):
+    def __init__(self, name: str, globals: Optional[Dict[str, Any]], level: int, parent:Optional["ImportedModule"]=None):
         self.name = name
         self.globals = globals
         self.level = level
         self.parent = parent
-        self.children = []
-        self.time = None
+        self.children: List["ImportedModule"]= []
+        self.time: float = 0
         if parent:
             parent.children.append(self)
 
-    def __eq__(self, value):
+    def __eq__(self, value:"ImportedModule") -> bool: #type: ignore
         return str(self) == str(value)
 
     @property
-    def all_children(self):
+    def all_children(self) -> List["ImportedModule"]:
         children = []
 
         for child in self.children:
@@ -36,13 +37,13 @@ class ImportedModule(object):
         return children
 
     @property
-    def own_time(self):
+    def own_time(self) -> float:
         """
         How many seconds it took to import this module, minus all child imports.
         """
         return self.time - sum(child.time for child in self.children)
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self.globals and self.level:
             if self.level == 1:
                 prefix = self.globals["__package__"]
@@ -53,10 +54,10 @@ class ImportedModule(object):
             prefix = ""
         return "{}{}".format(prefix, self.name)
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         self._start_time = time.time()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Optional[type], exc_val: Optional[Exception], exc_tb:TracebackType) -> None:
         self.time = time.time() - self._start_time
 
 
@@ -79,23 +80,23 @@ class ImportProfiler(object):
     to be imported.
     """
 
-    def __init__(self):
-        self._original_import = __builtins__["__import__"]
+    def __init__(self) -> None:
+        self._original_import = __builtins__["__import__"] #type: ignore
 
-    def __enter__(self):
-        __builtins__["__import__"] = self._profiled_import
-        self._top_imp_modules = []
-        self._import_stack = []
-        self.total_time = None
+    def __enter__(self) -> "ImportProfiler":
+        __builtins__["__import__"] = self._profiled_import #type:ignore
+        self._top_imp_modules: List[ImportedModule] = []
+        self._import_stack: List[ImportedModule]= []
+        self.total_time: float = 0
         self._start_time = time.time()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Optional[type], exc_val:Optional[Exception], exc_tb: Optional[TracebackType]) -> None:
         self.total_time = time.time() - self._start_time
-        __builtins__["__import__"] = self._original_import
+        __builtins__["__import__"] = self._original_import #type:ignore
 
     # def _profiled_import(self, name, globals=None, locals=None, fromlist=(), level=0):
-    def _profiled_import(self, name, globals=None, locals=None, fromlist=(), level=0):
+    def _profiled_import(self, name: str, globals: Dict[str, Any]=None,  locals: Dict[str, Any]=None, fromlist: Tuple=(), level: int=0) -> None:
         # print('Importing {}'.format(repr(name)))
         imp_mod = ImportedModule(
             name=name,
@@ -112,8 +113,8 @@ class ImportProfiler(object):
             finally:
                 self._import_stack.pop()
 
-    def print_stats(self, threshold_ms=0):
-        def print_imp_mod(imp_mod, indent=0):
+    def print_stats(self, threshold_ms: int=0) -> None:
+        def print_imp_mod(imp_mod: ImportedModule, indent: int=0) -> None:
             own_ms = int(imp_mod.own_time * 1000)
             if own_ms >= threshold_ms or any(
                 child
