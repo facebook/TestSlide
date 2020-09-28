3765c4028c6c5291937e84e5bae8ad47830f8438
Balint Csergo <death0wl@fb.com>
2020-09-28 23:53:18 +0100

    typing for mock_constructor

--- testslide/mock_constructor.py
+++ testslide/mock_constructor.py
@@ -5,7 +5,7 @@
 
 import gc
 import inspect
-from typing import Any, Callable, Dict, List
+from typing import Optional, Type, Union, Any, Callable, Dict, List, Tuple
 
 import testslide
 from testslide.mock_callable import _CallableMock, _MockCallableDSL
@@ -15,6 +15,7 @@ from .lib import (
     _validate_callable_arg_types,
     _validate_callable_signature,
 )
+
 
 _DO_NOT_COPY_CLASS_ATTRIBUTES = (
     "__dict__",
@@ -26,15 +27,15 @@ _DO_NOT_COPY_CLASS_ATTRIBUTES = (
 
 
 _unpatchers: List[Callable] = []
-_mocked_target_classes: Dict[int, type] = {}
-_restore_dict: Dict[int, Dict[str, Any]] = {}
-_init_args_from_original_callable = None
-_init_kwargs_from_original_callable = None
-_mocked_class_by_original_class_id: Dict[int, type] = {}
-_target_class_id_by_original_class_id: Dict[int, int] = {}
+_mocked_target_classes: Dict[Union[int, Tuple[int, str]], Tuple[type, object]] = {}
+_restore_dict: Dict[Union[int, Tuple[int, str]], Dict[str, Any]] = {}
+_init_args_from_original_callable: Optional[Tuple[Any, ...]]= None
+_init_kwargs_from_original_callable: Optional[Dict[str, Any]] = None
+_mocked_class_by_original_class_id: Dict[Union[Tuple[int, str], int], type] = {}
+_target_class_id_by_original_class_id: Dict[int, Union[Tuple[int, str], int]] = {}
 
 
-def _get_class_or_mock(original_class):
+def _get_class_or_mock(original_class: Any) -> Any:
     """
     If given class was not a target for mock_constructor, return it.
     Otherwise, return the mocked subclass.
@@ -42,11 +43,11 @@ def _get_class_or_mock(original_class):
     return _mocked_class_by_original_class_id.get(id(original_class), original_class)
 
 
-def _is_mocked_class(klass):
+def _is_mocked_class(klass: Type[object]) -> bool:
     return id(klass) in [id(k) for k in _mocked_class_by_original_class_id.values()]
 
 
-def unpatch_all_constructor_mocks():
+def unpatch_all_constructor_mocks() -> None:
     """
     This method must be called after every test unconditionally to remove all
     active patches.
@@ -62,16 +63,15 @@ class _MockConstructorDSL(_MockCallableDSL):
     """
     Specialized version of _MockCallableDSL to call __new__ with correct args
     """
-
     _NAME: str = "mock_constructor"
 
-    def __init__(self, target, method, cls, callable_mock=None, original_callable=None):
+    def __init__(self, target: Union[type, str, object], method: str, cls: object, callable_mock: Union[Optional[Callable[[Type[object]], Any]],Optional[_CallableMock]]=None, original_callable: Optional[Callable]=None):
         self.cls = cls
-        caller_frame = inspect.currentframe().f_back
+        caller_frame = inspect.currentframe().f_back #type: ignore
         # loading the context ends up reading files from disk and that might block
         # the event loop, so we don't do it.
         caller_frame_info = inspect.getframeinfo(caller_frame, context=0)
-        super(_MockConstructorDSL, self).__init__(
+        super(_MockConstructorDSL, self).__init__( # type: ignore
             target,
             method,
             caller_frame_info,
@@ -79,31 +79,31 @@ class _MockConstructorDSL(_MockCallableDSL):
             original_callable=original_callable,
         )
 
-    def for_call(self, *args, **kwargs):
-        return super(_MockConstructorDSL, self).for_call(
+    def for_call(self, *args: Any, **kwargs: Any) -> "_MockConstructorDSL": 
+        return super(_MockConstructorDSL, self).for_call( #type: ignore
             *((self.cls,) + args), **kwargs
         )
 
-    def with_wrapper(self, func):
-        def new_func(original_callable, cls, *args, **kwargs):
+    def with_wrapper(self, func: Callable) -> "_MockConstructorDSL":
+        def new_func(original_callable: Callable, cls: object, *args: Any, **kwargs: Any) -> Any:
             assert cls == self.cls
 
-            def new_original_callable(*args, **kwargs):
+            def new_original_callable(*args: Any, **kwargs: Any) -> Any:
                 return original_callable(cls, *args, **kwargs)
 
             return func(new_original_callable, *args, **kwargs)
 
-        return super(_MockConstructorDSL, self).with_wrapper(new_func)
+        return super(_MockConstructorDSL, self).with_wrapper(new_func) #type: ignore
 
-    def with_implementation(self, func):
-        def new_func(cls, *args, **kwargs):
+    def with_implementation(self, func: Callable) -> "_MockConstructorDSL":
+        def new_func(cls: object, *args: Any, **kwargs :Any) -> Any:
             assert cls == self.cls
             return func(*args, **kwargs)
 
-        return super(_MockConstructorDSL, self).with_implementation(new_func)
+        return super(_MockConstructorDSL, self).with_implementation(new_func) #type: ignore
 
 
-def _get_original_init(original_class, instance, owner):
+def _get_original_init(original_class: type, instance: object, owner: type) -> Any:
     target_class_id = _target_class_id_by_original_class_id[id(original_class)]
     # If __init__ available at the class __dict__...
     if "__init__" in _restore_dict[target_class_id]:
@@ -111,7 +111,7 @@ def _get_original_init(original_class, instance, owner):
         return _restore_dict[target_class_id]["__init__"].__get__(instance, owner)
     else:
         # otherwise, pull from a parent class.
-        return original_class.__init__.__get__(instance, owner)
+        return original_class.__init__.__get__(instance, owner) #type: ignore
 
 
 class AttrAccessValidation(object):
@@ -121,13 +121,13 @@ class AttrAccessValidation(object):
         "pointer to the new mocked class (eg: {}.{})."
     )
 
-    def __init__(self, name, original_class, mocked_class):
+    def __init__(self, name: str, original_class: type, mocked_class: type) -> None:
         self.name = name
         self.original_class = original_class
         self.mocked_class = mocked_class
 
-    def __get__(self, instance, owner):
-        mro = owner.mro()
+    def __get__(self, instance: Optional[type], owner: Type[type]) -> Union[Callable, str]:
+        mro = owner.mro() #type: ignore
         # If owner is a subclass, allow it
         if mro.index(owner) < mro.index(self.original_class):
             parent_class = mro[mro.index(self.original_class) + 1]
@@ -155,14 +155,14 @@ class AttrAccessValidation(object):
                 )
             )
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: object, value: Any) -> None:
         raise BaseException(
             self.EXCEPTION_MESSAGE.format(
                 "setting", self.original_class.__module__, self.original_class.__name__
             )
         )
 
-    def __delete__(self, instance):
+    def __delete__(self, instance: object) -> None:
         raise BaseException(
             self.EXCEPTION_MESSAGE.format(
                 "deleting", self.original_class.__module__, self.original_class.__name__
@@ -170,8 +170,8 @@ class AttrAccessValidation(object):
         )
 
 
-def _wrap_type_validation(template, callable_mock, callable_templates):
-    def callable_mock_with_type_validation(*args, **kwargs):
+def _wrap_type_validation(template: object, callable_mock: _CallableMock, callable_templates: List[Callable]) -> Callable:
+    def callable_mock_with_type_validation(*args: Any, **kwargs: Any) -> Any:
         for callable_template in callable_templates:
             if _validate_callable_signature(
                 False,
@@ -187,14 +187,14 @@ def _wrap_type_validation(template, callable_mock, callable_templates):
     return callable_mock_with_type_validation
 
 
-def _get_mocked_class(original_class, target_class_id, callable_mock, type_validation):
+def _get_mocked_class(original_class: type, target_class_id: Union[Tuple[int, str], int], callable_mock: _CallableMock, type_validation: bool) -> type:
     if target_class_id in _target_class_id_by_original_class_id:
         raise RuntimeError("Can not mock the same class at two different modules!")
     else:
         _target_class_id_by_original_class_id[id(original_class)] = target_class_id
 
     original_class_new = original_class.__new__
-    original_class_init = original_class.__init__
+    original_class_init = original_class.__init__ #type: ignore
 
     # Extract class attributes from the target class...
     _restore_dict[target_class_id] = {}
@@ -247,14 +247,14 @@ def _get_mocked_class(original_class, target_class_id, callable_mock, type_valid
     # Because __init__ is called after __new__ unconditionally with the same
     # arguments, we need to mock it fir this first call, to call the real
     # __init__ with the correct arguments.
-    def init_with_correct_args(self, *args, **kwargs):
+    def init_with_correct_args(self: object, *args: Any, **kwargs: Any) -> None:
         global _init_args_from_original_callable, _init_kwargs_from_original_callable
         if None not in [
             _init_args_from_original_callable,
             _init_kwargs_from_original_callable,
         ]:
-            args = _init_args_from_original_callable
-            kwargs = _init_kwargs_from_original_callable
+            args = _init_args_from_original_callable #type: ignore
+            kwargs = _init_kwargs_from_original_callable #type: ignore
 
         original_init = _get_original_init(
             original_class, instance=self, owner=mocked_class
@@ -265,19 +265,19 @@ def _get_mocked_class(original_class, target_class_id, callable_mock, type_valid
             _init_args_from_original_callable = None
             _init_kwargs_from_original_callable = None
 
-    mocked_class.__init__ = init_with_correct_args
+    mocked_class.__init__ = init_with_correct_args #type: ignore
 
     return mocked_class
 
 
 def _patch_and_return_mocked_class(
-    target, class_name, target_class_id, original_class, callable_mock, type_validation
-):
+    target: object, class_name: str, target_class_id: Union[Tuple[int, str], int], original_class: type, callable_mock: _CallableMock, type_validation: bool
+) -> type:
     mocked_class = _get_mocked_class(
         original_class, target_class_id, callable_mock, type_validation
     )
 
-    def unpatcher():
+    def unpatcher() -> None:
         for name, value in _restore_dict[target_class_id].items():
             setattr(original_class, name, value)
         del _restore_dict[target_class_id]
@@ -295,7 +295,7 @@ def _patch_and_return_mocked_class(
     return mocked_class
 
 
-def mock_constructor(target, class_name, allow_private=False, type_validation=True):
+def mock_constructor(target: str, class_name: str, allow_private: bool=False, type_validation: bool=True) -> _MockConstructorDSL:
     if not isinstance(class_name, str):
         raise ValueError("Second argument must be a string with the name of the class.")
     _bail_if_private(class_name, allow_private)
@@ -337,7 +337,7 @@ def mock_constructor(target, class_name, allow_private=False, type_validation=Tr
         elif not issubclass(original_class, object):
             raise ValueError("Old style classes are not supported.")
 
-        caller_frame = inspect.currentframe().f_back
+        caller_frame = inspect.currentframe().f_back #type: ignore
         # loading the context ends up reading files from disk and that might block
         # the event loop, so we don't do it.
         caller_frame_info = inspect.getframeinfo(caller_frame, context=0)
@@ -351,7 +351,7 @@ def mock_constructor(target, class_name, allow_private=False, type_validation=Tr
             type_validation,
         )
 
-    def original_callable(cls, *args, **kwargs):
+    def original_callable(cls: type, *args: Any, **kwargs: Any) -> Any:
         global _init_args_from_original_callable, _init_kwargs_from_original_callable
         assert cls is mocked_class
         # Python unconditionally calls __init__ with the same arguments as
