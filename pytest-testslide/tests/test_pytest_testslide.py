0be98604e0c5ad401b91ae521e4b2019f09bde5f
Balint Csergo <death0wl@fb.com>
2020-10-15 23:50:07 +0100

    add coverage for unpatching works, fix unpatching

--- pytest-testslide/tests/test_pytest_testslide.py
+++ pytest-testslide/tests/test_pytest_testslide.py
@@ -8,6 +8,8 @@ import re
 def test_pass(testdir):
     testdir.makepyfile(
         """
+		import time
+		import pytest
 		from pytest_testslide import testslide
 		from tests import sample_module
 		from testslide import StrictMock
@@ -45,6 +47,24 @@ def test_pass(testdir):
 		def test_patch_attribute_unpaches(testslide):
 			testslide.patch_attribute
 
+		def test_mock_callable_patching_works(testslide):
+			testslide.mock_callable("time", "sleep").to_raise(RuntimeError("Mocked!"))
+			with pytest.raises(RuntimeError):
+				time.sleep()
+
+		def test_mock_callable_unpatching_works(testslide):
+			# This will fail if unpatching from test_mock_callable_patching_works does
+			# not happen
+			time.sleep(0)
+
+		def test_mock_callable_assertion_works(testslide):
+			testslide.mock_callable("time", "sleep").for_call(0).to_call_original().and_assert_called_once()
+			time.sleep(0)
+
+		def test_mock_callable_failed_assertion_works(testslide):
+			testslide.mock_callable("time", "sleep").for_call(0).to_call_original().and_assert_called_once()
+			time.sleep(0)
+
 		def test_aggregated_exceptions(testslide):
 			mocked_cls = StrictMock(sample_module.CallOrderTarget)
 			testslide.mock_callable(mocked_cls, 'f1')\
@@ -57,17 +77,17 @@ def test_pass(testdir):
 		"""
     )
     result = testdir.runpytest("-v")
-    assert "12 passed, 1 error" in result.stdout.str()
+    assert "passed, 1 error" in result.stdout.str()
     expected_failure = re.compile(
         """.*_______________ ERROR at teardown of test_aggregated_exceptions ________________
 2 failures.
 <class \'AssertionError\'>: calls did not match assertion.
-<StrictMock 0x[a-fA-F0-9]+ template=tests.sample_module.CallOrderTarget .*/test_pass0/test_pass.py:39>, \'f1\':
+<StrictMock 0x[a-fA-F0-9]+ template=tests.sample_module.CallOrderTarget .*/test_pass0/test_pass.py:[0-9]+>, \'f1\':
   expected: called exactly 1 time\(s\) with arguments:
     \(\'a\',\)
   received: 0 call\(s\)
 <class \'AssertionError\'>: calls did not match assertion.
-<StrictMock 0x[a-fA-F0-9]+ template=tests.sample_module.CallOrderTarget .*/test_pass0/test_pass.py:39>, \'f1\':
+<StrictMock 0x[a-fA-F0-9]+ template=tests.sample_module.CallOrderTarget .*/test_pass0/test_pass.py:[0-9]+>, \'f1\':
   expected: called exactly 1 time\(s\) with arguments:
     \(\'b\',\)
   received: 0 call\(s\).*""",
