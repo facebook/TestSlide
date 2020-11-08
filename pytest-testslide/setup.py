adf12cf3e8f9b68bd1337477842e0adc361df43d
Balint Csergo <death0wl@fb.com>
2020-11-08 12:06:34 +0000

    bind version to actual testslide version

--- pytest-testslide/setup.py
+++ pytest-testslide/setup.py
@@ -3,7 +3,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 from setuptools import setup  # type: ignore
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+ts_version = open(os.path.join(dir_path, os.pardir, "testslide/version")).read().rstrip()
 
 with open("README.md", encoding="utf8") as f:
     readme = f.read()
@@ -21,7 +26,7 @@ setup(
     long_description_content_type="text/markdown",
     setup_requires=["setuptools>=38.6.0"],
     install_requires=[
-        "testslide>=2.2.1",
+        f"testslide>={ts_version}",
         "pytest>=5.3.0",
         "pytest-asyncio>=0.14.0",
     ],
