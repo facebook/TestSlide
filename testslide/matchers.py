790d4aa45ef46ed33ed15cb33b1270673519d92f
Benjamin Jones <bpj@fb.com>
2023-03-27 15:43:31 -0700

    make format

--- testslide/matchers.py
+++ testslide/matchers.py
@@ -3,8 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import re
+from typing import Any as AnyType
 from typing import (
-    Any as AnyType,
     Callable,
     Container,
     Dict,
