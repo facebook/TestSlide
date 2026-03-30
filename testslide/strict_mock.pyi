673df25fce4a662365b8c9e2db56e540510d22ba
Maggie Moss <maggiemoss@meta.com>
2026-03-30 16:31:25 -0700

    Revert D97807972
    Summary:
    This diff reverts D97807972
    (The context such as a Sandcastle job, Task, SEV, etc. was not provided.)
    
    Depends on D97807972
    
    Reviewed By: karlbloedorn, diggyk
    
    Differential Revision: D98157728
    
    fbshipit-source-id: 6c93dbc8ac8093802a4df2954ee36f9e6c6bc8ee

--- testslide/strict_mock.pyi
+++ testslide/strict_mock.pyi
@@ -1,13 +1,4 @@
 # pyre-unsafe
 from typing import Any
 
-class StrictMock(Any):
-    def __init__(
-        self,
-        template: type | None = None,
-        runtime_attrs: list[Any] | None = None,
-        name: str | None = None,
-        default_context_manager: bool = False,
-        type_validation: bool = True,
-        attributes_to_skip_type_validation: list[str] = [],
-    ) -> None: ...
+StrictMock = Any
